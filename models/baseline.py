3f8c71f4765816c9fbfc045ed7eb129df9c72430
Robs98B <bonini98@gmail.com>
2023-02-23 19:11:50 +0100

    Update baseline.py

--- models/baseline.py
+++ models/baseline.py
@@ -12,7 +12,8 @@ class BertBaselineClassifier(pl.LightningModule):
         self.num_labels = num_labels
         self.config = AutoConfig.from_pretrained(model_name)
         self.config.num_labels = num_labels
-
+        
+        self.batch_size = config["batch_size"]
         classifier_dropout = config["classifier_dropout"]
 
         self.config.classifier_dropout = classifier_dropout
