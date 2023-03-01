f13f7ce3482622b0a2bd3c2c2ea6a5f3471df4ab
maidacundo <maidacundo@gmail.com>
2023-03-01 14:41:11 +0100

    data augmentation implementation

--- utils/data.py
+++ utils/data.py
@@ -14,6 +14,52 @@ def get_processed_df(input_df, labels_columns, tokenizer_sep_token):
 
     return df
 
+
+def format_batch_texts(language_code, batch_texts):
+  
+  formated_bach = [">>{}<< {}".format(language_code, text) for text in batch_texts]
+
+  return formated_bach
+
+def perform_translation(batch_texts, model, tokenizer, language="fr", device='cuda:0'):
+    # Prepare the text data into appropriate format for the model
+    formated_batch_texts = format_batch_texts(language, batch_texts)
+    
+    # Generate translation using model
+    translated = model.generate(**tokenizer(formated_batch_texts, return_tensors="pt", padding=True).to(device))
+
+    # Convert the generated tokens indices back into text
+    translated_texts = [tokenizer.decode(t, skip_special_tokens=True) for t in translated]
+    
+    return translated_texts
+
+def perform_data_augmentation(df_train_aumentation, first_model, second_model, first_model_tkn, second_model_tkn, batch_size=8, columns=['Premise', 'Conclusion'], device='cuda:0'):
+    first_model.to(device)
+    second_model.to(device)
+    for column in columns:
+        original_texts = df_train_aumentation[column].to_list()
+        dataloader = DataLoader(original_texts, batch_size=batch_size)
+        translated_texts = []
+        print(f'translating {column}')
+        for batch in dataloader:
+            translated_batch = perform_translation(batch, first_model, first_model_tkn)
+            for text in translated_batch:
+                translated_texts.append(text)
+
+        dataloader = DataLoader(translated_texts, batch_size=batch_size)
+
+        back_translated_texts = []
+        print(f'back-translating {column}')
+        for batch in dataloader:
+            back_translated_batch = perform_translation(batch, second_model, second_model_tkn)
+            for text in back_translated_batch:
+                back_translated_texts.append(text)
+        df_train_aumentation[f'{column} BT'] = back_translated_texts
+    return df_train_aumentation
+
+
+
+
 class HumanValuesDataset(Dataset):
 
   def __init__(
--- utils/trainer.py
+++ utils/trainer.py
@@ -1,1 +1,58 @@
 from pytorch_lightning.callbacks import ModelCheckpoint, EarlyStopping
+import optuna
+import torch
+import pytorch_lightning as pl
+from utils.data import HumanValuesDataModule
+
+class HumanValueTrainer:
+    def __init__(self, model, data_module):
+        
+        self.data_module = data_module
+        self.model = model
+        
+        
+    def objective(self, trial: optuna.Trial):
+        lr = trial.suggest_float("lr", 1e-6, 1e-3)
+        batch_size = trial.suggest_categorical("batch_size", [8])
+        optimizer = trial.suggest_categorical("optim", [torch.optim.Adam, torch.optim.AdamW])
+        classifier_dropout = trial.suggest_categorical("optimizer_dropout", [.1, .2, .3])
+        max_tok_len = trial.suggest_categorical("max_tok_len", [128, 256, 300, 368])
+
+        self.data_module.batch_size=batch_size
+        self.data_module.max_token_len=max_tok_len
+        self.data_module.setup()
+
+        self.model.optim = optimizer
+        self.model.lr = lr
+        self.model.classifier_dropout = classifier_dropout
+
+
+        early_stopping_callback = EarlyStopping(monitor='val_loss', patience=3)
+        logger = pl.loggers.TensorBoardLogger("lightning_logs", name="human-values")
+
+        trainer = pl.Trainer(
+            logger=logger,
+            callbacks=[early_stopping_callback],
+            max_epochs=self.n_epochs,
+            limit_train_batches=self.lim_train_batches,
+            limit_val_batches=self.lim_val_batches,
+            accelerator='gpu',
+            devices=1
+        )
+
+        trainer.fit(self.model, self.data_module)
+        value = trainer.callback_metrics["val_loss"].item()
+
+        if trial.should_prune():
+            raise optuna.TrialPruned()
+
+        return value
+
+    def run_study(self):
+        self.study = optuna.create_study(direction="minimize")
+        self.study.optimize(self.objective, n_trials=self.n_trials)
+
+    def get_best_hyperparams(self):
+        if self.study is None:
+            raise Exception("Please run the study first.")
+        return self.study.best_params
