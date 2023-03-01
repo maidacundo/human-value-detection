c6cf8ac50eef26e3986b197a73396f09a06e9eac
Robs98B <bonini98@gmail.com>
2023-03-01 03:57:54 +0100

    add spv model

---
+++ models/bert_spv.py
@@ -0,0 +1,34 @@
+import torch
+import pytorch_lightning as pl
+from transformers import AutoConfig, BertModel, AdamW, get_linear_schedule_with_warmup
+import torch.nn as nn
+import torchmetrics
+
+class BertSVMmodel(pl.LightningModule):
+    def __init__(self, model_name):
+        #lr=2e-5, classifier_dropout=.1):
+        super().__init__()
+        self.bert = BertModel.from_pretrained(model_name)
+
+    def forward(
+        self,
+        input_ids=None,
+        attention_mask=None,
+    ):
+        with torch.no_grad():
+            outputs = self.bert(
+                input_ids,
+                attention_mask=attention_mask
+            )
+
+        # verificare che cos'è il pooled output (in realtà conviene verificare che cos'è tutto l'output)
+        embeddings = outputs[0][:,0,:].numpy()
+
+        return embeddings 
+    
+    def train_step(self, batch):
+        input_ids = batch["input_ids"]
+        attention_mask = batch["attention_mask"]
+        outputs = self(input_ids, attention_mask)
+
+        return outputs
