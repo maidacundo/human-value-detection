2b9e317cea0f014bc290584ab819a7948f2d8abc
Robs98B <bonini98@gmail.com>
2023-03-25 22:45:16 +0100

    Update bert_svm.py

--- models/bert_svm.py
+++ models/bert_svm.py
@@ -1,12 +1,9 @@
-import torch
 import pytorch_lightning as pl
-from transformers import AutoConfig, BertModel, AdamW, get_linear_schedule_with_warmup
+from transformers import BertModel
 import torch.nn as nn
-import torchmetrics
 
 class BertSVMmodel(pl.LightningModule):
     def __init__(self, model_name):
-        #lr=2e-5, classifier_dropout=.1):
         super().__init__()
         self.bert = BertModel.from_pretrained(model_name)
 
@@ -24,11 +21,11 @@ class BertSVMmodel(pl.LightningModule):
         cls = outputs[0][:,0,:]
         pooler = outputs.pooler_output
 
-        return cls, pooler
+        return (cls, pooler)
     
     def predict_step(self, batch, batch_idx):
         input_ids = batch["input_ids"]
         attention_mask = batch["attention_mask"]
-        cls, pooler = self(input_ids, attention_mask)
+        predictions = self(input_ids, attention_mask)
 
-        return cls, pooler
+        return predictions[0], predictions[1]
